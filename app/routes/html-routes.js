403788e2f7cf41c9225961506af5bfa82f3b0c27
Anthony Velardo <avelardo93@gmail.com>
2016-07-12 18:55:17 -0400

    express routing

---
+++ app/routes/html-routes.js
@@ -0,0 +1,39 @@
+var path = require('path');
+
+module.exports = function(app){
+
+	// HTML GET Requests
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases the user is shown an HTML page of content
+	// ---------------------------------------------------------------------------
+
+	app.get('/', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
+	});
+
+	app.get('/backhealth', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/backhealth.html'));
+	});
+
+	app.get('/contact', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/contact.html'));
+	});
+
+	app.get('/testimonial', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/testimonial.html'));
+	});
+
+	app.get('/NewPatient', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/NewPatient.html'));
+	});
+
+	app.get('/Newsletter', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/Newsletter.html'));
+	});
+
+	// If no matching route is found default to home
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
+	});
+
+}

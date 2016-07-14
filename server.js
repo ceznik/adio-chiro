2e58bdb6eff220b043d6eddb8a9e6034cf22db9e
James <semaj5588@gmail.com>
2016-07-13 21:47:48 -0400

    added express server and updated contact page and css file

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var app = express();
+var PORT = process.env.PORT || 3000; 	// set the port
+var staticContentFolder;
+
+//some basic config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+// app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT,function() {
+	console.log('App listening on PORT: '+ PORT);
+});
+
+app.get('/', function (req, res){
+	res.send("this is my contact page");
+})
+
+app.post('/asdf', function(req, res){
+	console.log('this is working');
+	console.log(req.body);
+
+
+})
